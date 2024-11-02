45cfdaa18fe47f301ecf932f468b31c740f30600
James Kelly <jkelly@sensoryinc.com>
2024-11-01 22:59:28 -0700

    OPTIMIZATIONS

--- ishihara_gen.py
+++ ishihara_gen.py
@@ -138,7 +138,7 @@ class IshiharaPlateGenerator:
             8    # Tiny - for filling gaps
         ]
         # Weights based on sample image analysis
-        self.size_weights = [0.002, 0.008, 0.05, 0.20, 0.35, 0.25, 0.11, 0.03]  # Adds to 1.0
+        self.size_weights = [0.01, 0.008, 0.05, 0.17, 0.26, 0.2, 0.1, 0.03]  # Adds to 1.0
         return [s//2 for s in sizes]  # Convert to radii
 
 
